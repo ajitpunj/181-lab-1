1f33ed0a3d27a971714adb105e1bfb481e0f49d1
ajitpunj <apunj@ucdavis.edu>
2015-01-12 19:00:54 -0800

    final code

--- Hex_LED.c
+++ Hex_LED.c
@@ -1,4 +1,4 @@
-// Simple test program
+//Part 3 full code
 
 
 int main(void)
@@ -13,53 +13,153 @@ int main(void)
 	int * hex3_hex0_test = (int*) 0xFF200020;
 
 	int switch_value;
-	int pushbuttons_value;
+	int pushbuttons_value, pushbuttons_value2;
+	int i,n,true,pause,c,l,p, temp, checkPaused, hold, bob;
+	int speed;
+	int increment; 
+	int first_part, second_part;
+ 
+	int array_state_1[13] = {943210240, 943652926, 1056980542, 4079167, 1044266832, 1044336696, 1062221918, 1345871360, 945684598, 1577088633, 7764280, 1987655736, 2033727551};
+	int array_state_2[13] = {30329, 31032, 14392, 14399, 16128, 62, 15934, 15935, 16208, 20536, 14430, 24064, 118}; 
 
-	bool phrase = true; 
-		
-	int array_state_1[ = {0, 118,
-	int arrat_state_2[ = {  
-
-	while( 1 )
-	{ 
-		pushbuttons_value = *(pushbuttons);
-
-		if (pushbuttons_value == 8){
-			continue;
-		}
-
-		if (pushbuttons_value == 1){
-			phrase = !phrase;
-		}
-
-		while (phrase == true)
-		{
+	int array_state_3[4] = {256,512,768,1024};
+	speed = 5;
+	true = 0;
+	pause = 0;
+	hold = 0;
+	l = 0;
+	i = 0;
+	checkPaused = 0;
+	while(1){
+				
+			// Hello World Loop 
+			
+			i = i % 13;
+			l = l % 4;
 			pushbuttons_value = *(pushbuttons);
+			
 			if (pushbuttons_value == 8){
-				continue;
+				true = !true; 
+				for (p=0; p <(20*(10000));p++){
+			
+					switch_value = *(switchptr);
+					*(led) = switch_value;
+				}
 			}
 			if (pushbuttons_value == 1){
-				phrase = ~phrase;
-				break;
+				/*hold = !hold;
+				if (hold==1)
+					continue;*/
+				checkPaused=1;
+				/*while(checkPaused){
+					pushbuttons_value=*(pushbuttons);
+					if(pushbuttons_value==1){
+						checkPaused=0;
+					}
+				}
+				temp = speed;*/ 
+				//speed = 10000;
+				for (c=0; c <(30*(10000));c++){
+			
+					switch_value = *(switchptr);
+					*(led) = switch_value;
+				}/*
+				for (c=0; c <(20*(10000));c++){
+			
+					switch_value = *(switchptr);
+					*(led) = switch_value;
+				}
+				continue;*/
 			}
+			while(checkPaused==1){
+				int backtozero;
+				pushbuttons_value2 = *(pushbuttons);
+				//initially, wait for pushbuttons to go back to 0 state
+				if(pushbuttons_value2==0)
+					backtozero=1;
+				//after button is unpressed, check for value to be 1 again
+				if(pushbuttons_value2==1 && backtozero==1){
+					checkPaused=0;
+					for (c=0; c <(70*(10000));c++){
+			
+					switch_value = *(switchptr);
+					*(led) = switch_value;
+					}
+				}
+			}
+			
+				
 			switch_value = *(switchptr);
 			*(led) = switch_value;
-
-			if (pushbuttons_value == 1 ){
 			
-				*(hex3_hex0) = 118;		//H = 118
-				*(hex3_hex0) = 16246;
- 			}
-			if (pushbuttons_value == 8){
-				*(hex3_hex0) = 0;
+			if (true==0){
+				*(hex3_hex0) = array_state_1[i];		
+				*(hex5_hex4) = array_state_2[i];
+				i = i + 1;
 			}
-		}
-
-		if (phrase == false) 
-		{
-			//switch mode
-		}
- 
+			else{
+				first_part = switch_value/(128);
+				first_part = first_part*256;
+				second_part = switch_value%256;
+				bob = multi(l);
+				*(hex3_hex0) = (first_part+second_part)*(bob);
+				*(hex5_hex4) = 0;
+				l = l + 1;
+			}
+			
+			if (pushbuttons_value==4){
+				speed = speed - 1;
+			}
+				
+			if (pushbuttons_value==2){
+				speed = speed + 1;
+			}
+			
+			
+			for (n=0; n <(speed*(10000));n++){
+				pushbuttons_value = *(pushbuttons);
+				/*if (pushbuttons_value==1){
+					
+					speed = temp;
+					break;
+					
+				} */
+				switch_value = *(switchptr);
+				*(led) = switch_value;
+			}
+			/*while(1){
+				pushbuttons_value = *(pushbuttons);
+				if(checkPaused==1){
+					if (pushbuttons_value == 1){
+						checkPaused = 0;
+					}
+				}
+			
+				if (checkPaused == 0){				
+					while(1){
+						pushbuttons_value = *(pushbuttons);
+						if (pushbuttons_value==0)
+							break;
+					}
+					break;
+				}
+				
+			}*/
+			
+			
+		
 	}
-	
 }
+
+int multi(int l){
+	int c;
+	int output;
+	output = 1;
+	for (c=0; c<l;c++){
+		output = output*256;
+	}
+	return (output);
+}
+
+
+
